9c1f5f07eb0741bd6ee8175d9b5577c8cd856b87
rokman123 <rokismank@gmail.com>
2023-12-07 19:22:10 +0200

    Add chainOfRespobsibility

---
+++ PacmanGame(WinForms)/ChainOfResponsibility/IHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace PacmanGame_WinForms_.ChainOfResponsibility
+{
+    public interface IHandler
+    {
+         IHandler SetNext(IHandler handler);
+
+         void Handle(Ghost ghost);
+    }
+}
